4c98fe9f4097abffa3af38264fd2f8b1eac2acdc
xiao429887822 <429887822@qq.com>
2025-08-19 02:21:14 +0000

    Deploying to gh-pages from @ xiao429887822/Yunnav@350632d3a4160cffd2062dd7a8444631fd977b50 🚀

---
+++ chunk-UPRRYMX5.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-5TYA3NRZ.js";import{pb as r,yb as e}from"./chunk-UHYW6ZWY.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
